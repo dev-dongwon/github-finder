845f42b561d85cc74dcfaa33e09fd109df5331b2
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-21 22:33:31 +0900

    Feat : user 세부 페이지 레이아웃, 정보 추가

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Spinner from "../layout/Spinner";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export class User extends Component {
   componentDidMount() {
@@ -11,8 +11,8 @@ export class User extends Component {
   static propTypes = {
     loading: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-  }
+    getUser: PropTypes.func.isRequired
+  };
 
   render() {
     const {
@@ -27,16 +27,81 @@ export class User extends Component {
       following,
       public_repos,
       public_gists,
-      hireable
+      hireable,
+      company
     } = this.props.user;
 
     const { loading } = this.props;
 
-    if (loading) return <Spinner />
+    if (loading) return <Spinner />;
 
-    return <Fragment>
-      <Link to='/' className='btn btn-light'>Back to search</Link>
-    </Fragment>;
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back to search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=""
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong><a href={blog}>{blog}</a>
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+
+          <div className="card text-center">
+            <div className="badge badge-primary">Followers: {followers}</div>
+            <div className="badge badge-success">Following: {following}</div>
+            <div className="badge badge-light">Public Repos: {public_repos}</div>
+            <div className="badge badge-dark">public Gists: {public_gists}</div>
+
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
