ecd2cef0626990337946587b4f9523a0dfb3eecc
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-21 18:41:24 +0900

    Feat : Add method to get users through axios

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,12 +1,12 @@
 import React from "react";
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const UserItem = ({ user : { login, avatar_url, html_url }}) => {
   return (
     <div className="card text-center">
       <img
         src={avatar_url}
-        alt="avatar-photo"
+        alt="avatar"
         className="round-img"
         style={{ width: "60px" }}
       />
