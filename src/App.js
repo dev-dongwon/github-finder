257eede8eecf96de436ec7518aca0729434982e8
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-21 19:59:30 +0900

    Feat : Add passing Props up & Search

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import "./App.css";
+import Search from "./components/users/Search";
 
 class App extends Component {
-
   state = {
     users: [],
     loading: false
-  }
+  };
 
-  async componentDidMount() {
-    this.setState({ loading : true });
+  SearchUsers = async text => {
+    this.setState({ loading: true })
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
-    this.setState({ users: res.data, loading: false });
-  }
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar />
-          <div className="container">
-            <Users loading={this.state.loading} users={this.state.users} />
-          </div>
+        <div className="container">
+          <Search SearchUsers={this.SearchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
