e99ba9971facdb79d1ed0b746564e41f44b1a62f
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-21 14:23:35 +0900

    Feat : Add navbar with default props
    defaultProps로 navbar props 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from react</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
