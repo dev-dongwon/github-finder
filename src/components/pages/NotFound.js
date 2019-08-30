297cf4e7f6ba8e496cf75587663d01b5ad090a90
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-30 15:07:59 +0900

    Modified : local env와 production env 분리
    배포 준비를 위해 로컬 환경 변수와 프로덕션 환경변수 분리

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className="lead">페이지가 존재하지 않습니다</p>
+    </div>
+  )
+}
+
+export default NotFound
